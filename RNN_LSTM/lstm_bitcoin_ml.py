566442d11e99a245bee321e53fbd01b5940dcfae
nathanduft44 <nathan.duft@gmail.com>
2021-10-10 17:05:04 -0700

    added a Prediction using lstm for BTC/USD 2018-2021

--- RNN_LSTM/lstm_bitcoin_ml.py
+++ RNN_LSTM/lstm_bitcoin_ml.py
@@ -19,7 +19,7 @@ import seaborn as sns
 from sklearn.metrics import mean_absolute_error
 # %matplotlib inline
 
-btc_df = pd.read_csv("daily_btc_ohclv_2021_lstm", index_col= "Open Time")
+btc_df = pd.read_csv("btc_daily_ohlcv_2016to2021.csv", index_col= "Open Time")
 target_col = 'Close'
 
 # btc_df = pd.read_csv("minute_btc_ohclv", index_col = "Open Time")
@@ -46,7 +46,7 @@ def line_plot(line1, line2, label1=None, label2=None, title='', lw=2):
     fig, ax = plt.subplots(1, figsize=(13, 7))
     ax.plot(line1, label=label1, linewidth=lw)
     ax.plot(line2, label=label2, linewidth=lw)
-    ax.set_ylabel('price [CAD]', fontsize=14)
+    ax.set_ylabel('price [USD]', fontsize=14)
     ax.set_title(title, fontsize=16)
     ax.legend(loc='best', fontsize=16)
 
